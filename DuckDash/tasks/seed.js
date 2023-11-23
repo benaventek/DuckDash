1819860f878ae82052c66023f27e35c464282c11
benaventek <benavente.kev@gmail.com>
2023-11-22 19:03:26 -0500

    added seed script in package.json

--- DuckDash/tasks/seed.js
+++ DuckDash/tasks/seed.js
@@ -1,40 +1,42 @@
-import { dbConnection, closeConnection } from "../config/mongoConnection.js";
-import users from "../data/users.js";
+import { dbConnection, closeConnection } from '../config/mongoConnection.js';
+import users from '../data/users.js';
 
 const db = await dbConnection();
 
-db.dropDatabase();
+await db.dropDatabase();
 
 await users.addUser(
-  "test",
-  "Dremike6027@getNamedMiddlewareRegex.com",
-  "ThisisValid1?",
-  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-  "This is a test user"
+  'test',
+  'Dremike6027@getNamedMiddlewareRegex.com',
+  'ThisisValid1?',
+  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  'This is a test user'
 );
 await users.addUser(
-  "test2",
-  "Dremike6027@getNamedMiddlewareRegex.com",
-  "ThisisValid1?",
-  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-  "This is a test user"
+  'test2',
+  'Dremike6027@getNamedMiddlewareRegex.com',
+  'ThisisValid1?',
+  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  'This is a test user'
 );
-await users.updateUser("test", "Bio", "Hello I am a test user");
+await users.updateUser('test', 'Bio', 'Hello I am a test user');
 await users.updateUser(
-  "test",
-  "ProfilePictureUrl",
-  "DuckDash/public/LocalImages/defaultProfilePicture.png"
-);
-await users.updateUser(
-  "test",
-  "ProfilePictureUrl",
-  "DuckDash/public/LocalImages/defaultProfilePicture.png"
+  'test',
+  'ProfilePictureUrl',
+  'DuckDash/public/LocalImages/defaultProfilePicture.png'
 );
 await users.updateUser(
-  "test",
-  "ProfilePictureUrl",
-  "DuckDash/public/LocalImages/defaultProfilePicture.png"
+  'test',
+  'ProfilePictureUrl',
+  'DuckDash/public/LocalImages/defaultProfilePicture.png'
+);
+await users.updateUser(
+  'test',
+  'ProfilePictureUrl',
+  'DuckDash/public/LocalImages/defaultProfilePicture.png'
 );
 
-await users.updateUser("test", "FriendsList", "test2");
+await users.updateUser('test', 'FriendsList', 'test2');
+
+console.log('Done seeding database');
 await closeConnection();
