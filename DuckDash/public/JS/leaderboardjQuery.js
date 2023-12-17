06b10ec235a084e59f0373e737f1c50002033dbd
Andres Fernandez <94069766+Afernan413@users.noreply.github.com>
2023-12-17 11:55:24 -0500

    Fixed some css errors
    Fixed some css errors

--- DuckDash/public/JS/leaderboardjQuery.js
+++ DuckDash/public/JS/leaderboardjQuery.js
@@ -1,89 +1,78 @@
 (function ($) {
-    let userList = $('#userList'),
-        wpmButton = $('#wpm'),
-        accButton = $('#accuracy');
-        h3title = $('#sortingMethod');
-        errorArea = $('#error');
-    
-    function bindEventToProfile(userItem) {
-        userItem.find('.ProfilePicture, .userNameFromRequest').on('click', function (event) {
-            event.preventDefault();
-            let currentUsername = userItem.find('.userNameFromRequest').text();
+  let userList = $("#userList"),
+    wpmButton = $("#Leaderboardwpm"),
+    accButton = $("#Leaderboardaccuracy");
+  h3title = $("#sortingMethod");
+  errorArea = $("#error");
 
-            window.location.href = '/profile/' + currentUsername;
+  function bindEventToProfile(userItem) {
+    userItem
+      .find(".ProfilePicture, .userNameFromRequest")
+      .on("click", function (event) {
+        event.preventDefault();
+        let currentUsername = userItem.find(".userNameFromRequest").text();
+
+        window.location.href = "/profile/" + currentUsername;
+      });
+  }
+
+  userList.children().each(function (index, element) {
+    bindEventToProfile($(element));
+  });
+
+  wpmButton.on("click", function () {
+    errorArea.hide();
+    let requestConfig = {
+      method: "POST",
+      url: "/leaderboard/wpm",
+    };
+
+    $.ajax(requestConfig)
+      .done(function (responseMessage) {
+        console.log(responseMessage);
+        userList.html(responseMessage);
+        userList.children().each(function (index, element) {
+          bindEventToProfile($(element));
         });
-    }
+        h3title.text("WPM");
+      })
+      .fail(function (jqXHR, status, error) {
+        if (jqXHR.responseText) {
+          let responseHtml = $.parseHTML(jqXHR.responseText);
+          let errorContent = $(responseHtml).find("#error").html();
 
+          // Display the extracted error content in the 'errorArea'
+          errorArea.html(errorContent);
+        }
+      });
+  });
 
-    userList.children().each(function (index, element) {
-        bindEventToProfile($(element));
-    })
+  accButton.on("click", function () {
+    errorArea.hide();
+    let requestConfig = {
+      method: "POST",
+      url: "/leaderboard/accuracy",
+    };
 
-    wpmButton.on('click', function () {
-        errorArea.hide();
-        let requestConfig = {
-            method: 'POST',
-            url: '/leaderboard/wpm'
-        };
-
-        $.ajax(requestConfig)
-        .done(function (responseMessage) {
-            console.log(responseMessage);
-            userList.html(responseMessage);
-            userList.children().each(function (index, element) {
-                bindEventToProfile($(element));
-            });
-            h3title.text('WPM');
-            
-
-        })
-        .fail(function (jqXHR, status, error) {
-            if(jqXHR.responseText){
-                let responseHtml = $.parseHTML(jqXHR.responseText);
-                let errorContent = $(responseHtml).find('#error').html();
-            
-
-                // Display the extracted error content in the 'errorArea'
-                errorArea.html(errorContent);
-            }
-        })
-
-    });
-
-    accButton.on('click', function () {
-        errorArea.hide();
-        let requestConfig = {
-            method: 'POST',
-            url: '/leaderboard/accuracy'
-        };
-
-        $.ajax(requestConfig)
-        .done(function (responseMessage) {
-            console.log(responseMessage);
-            userList.html(responseMessage);
-            userList.children().each(function (index, element) {
-                bindEventToProfile($(element));
-            });
-            h3title.text('Accuracy');
-            
-
-        })
-        .fail(function (xhe, status, error) {
-            if(jqXHR.responseText){
-                let responseHtml = $.parseHTML(jqXHR.responseText);
-                let errorContent = $(responseHtml).find('#error').html();
-            
-
-                // Display the extracted error content in the 'errorArea'
-                errorArea.html(errorContent);
-                
-                errorArea.show();
-            }
+    $.ajax(requestConfig)
+      .done(function (responseMessage) {
+        console.log(responseMessage);
+        userList.html(responseMessage);
+        userList.children().each(function (index, element) {
+          bindEventToProfile($(element));
         });
+        h3title.text("Accuracy");
+      })
+      .fail(function (xhe, status, error) {
+        if (jqXHR.responseText) {
+          let responseHtml = $.parseHTML(jqXHR.responseText);
+          let errorContent = $(responseHtml).find("#error").html();
 
-    });
+          // Display the extracted error content in the 'errorArea'
+          errorArea.html(errorContent);
 
-    
-    
-
-})(window.jQuery)
+          errorArea.show();
+        }
+      });
+  });
+})(window.jQuery);
