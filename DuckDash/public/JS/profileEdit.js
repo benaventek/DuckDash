74ccb53453e9df75a5aeb1e8abf13a3d0c67df90
Andres Fernandez <94069766+Afernan413@users.noreply.github.com>
2023-12-17 21:06:40 -0500

    Added client side input validation
    Added client side input validation

--- DuckDash/public/JS/profileEdit.js
+++ DuckDash/public/JS/profileEdit.js
@@ -10,6 +10,22 @@ let bioInput = document.getElementById("bioInput");
 let pictureUploadArea = document.getElementById("uploadPicture");
 let pictureInput = document.getElementById("imageUpload");
 
+let commentInputArea = document.getElementById("addComment");
+let commentInput = document.getElementById("commentInput");
+
+commentInputArea.addEventListener("submit", async (event) => {
+  if (commentInput.value.length > 250) {
+    alert("Comment must be less than 250 characters");
+    event.preventDefault();
+    return;
+  }
+  if (commentInput.value.length < 1) {
+    alert("Comment must be at least 1 character");
+    event.preventDefault();
+    return;
+  }
+});
+
 editProfilePicture.addEventListener("click", () => {
   pictureUploadArea.hidden = false;
   CancelEditProfilePicture.hidden = false;
--- DuckDash/public/JS/profile_id.js
+++ DuckDash/public/JS/profile_id.js
@@ -1,1 +1,16 @@
 //TODO : do something with the friend request button, also friends list.
+let commentInputArea = document.getElementById("addComment");
+let commentInput = document.getElementById("commentInput");
+
+commentInputArea.addEventListener("submit", async (event) => {
+  if (commentInput.value.length > 250) {
+    alert("Comment must be less than 250 characters");
+    event.preventDefault();
+    return;
+  }
+  if (commentInput.value.length < 1) {
+    alert("Comment must be at least 1 character");
+    event.preventDefault();
+    return;
+  }
+});
