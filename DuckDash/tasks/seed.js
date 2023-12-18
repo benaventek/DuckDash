187cd7d834799f699d2e86b130849c91b9960958
Kevin Benavente <benavente.kev@gmail.com>
2023-12-17 22:17:09 -0500

    final seed

--- DuckDash/tasks/seed.js
+++ DuckDash/tasks/seed.js
@@ -1,13 +1,13 @@
 import { dbConnection, closeConnection } from '../config/mongoConnection.js';
 import users from '../data/users.js';
 import presetTests from '../data/presetTests.js';
-import results from '../data/results.js';
 const db = await dbConnection();
 
-await users.addUser('dremike1', 'test@gmail.com', 'Password#1');
-await users.addUser('dremike12', 'test2@gmail.com', 'Password#12');
-await users.addUser('dremike13', 'test3@gmail.com', 'Password#13');
-await users.addUser('dremike14', 'test4@gmail.com', 'Password#14');
+console.log('Creating users and preset tests...');
+await users.addUser('keb', 'keb@gmail.com', 'Password1!');
+await users.addUser('bestTyper', 'ilied@gmail.com', 'Password2!');
+await users.addUser('chronicallyOnline', 'sleep@gmail.com', 'Password3!');
+await users.addUser('gamer123', 'bestgamer@gmail.com', 'Password4!');
 
 await presetTests.CreateTypingTest(
   'Honor Pledge',
@@ -40,17 +40,6 @@ await presetTests.CreateTypingTest(
   'Easy',
   15
 );
-// await results.addResult(
-//   'preset',
-//   '6574e7b84edd0bd46702797b',
-//   '6574e9c677a449ce53769656',
-//   100,
-//   100,
-//   '12/16/2020',
-//   100
-// );
-
-// console.log(await results.getResultsBydisplayname('Dremike6027'));
 
 console.log('Done seeding database');
 await closeConnection();
